24321e7409057760ba6a7b085965ad6232b77ffd
Humberto Sandmann <hsandmann@ieee.org>
2024-08-13 12:42:32 -0300

    initial version

---
+++ src/main/java/espm/aula02/Ex1.java
@@ -0,0 +1,19 @@
+package espm.aula02;
+
+import java.util.Arrays;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        int ini = Integer.parseInt(args[0]);
+        int fim = Integer.parseInt(args[1]);
+        long total = 0l;
+        for (int i = ini; i < fim; i ++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                total += i;
+            } // 233168
+        }
+        System.out.println(total);
+    }
+    
+}
---
+++ src/main/java/espm/aula02/Hello.java
@@ -0,0 +1,9 @@
+package espm.aula02;
+
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
